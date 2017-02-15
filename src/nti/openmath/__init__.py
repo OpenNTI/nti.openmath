d5b78533e329f266e5be24315147d168d8041073
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-15 19:09:31 +0000

    add nose2 config; pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.openmath/trunk@106639 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/openmath/__init__.py
+++ src/nti/openmath/__init__.py
@@ -18,170 +18,179 @@ OMI = 'OMI'
 OMF = 'OMF'
 OMOBJ = 'OMOBJ'
 
+
 def binaryOperator(op, arg1, arg2):
-	return '%s %s %s' % (arg1, op, arg2)
+    return '%s %s %s' % (arg1, op, arg2)
+
 
 def unaryOperator(op, arg1):
-	return '%s%s' % (op, arg1)
+    return '%s%s' % (op, arg1)
+
 
 def latexMacro(macro, *args):
-	result = ['%s' % macro]
-	for arg in args:
-		result.append('{%s}' % arg)
-	return ''.join(result)
+    result = ['%s' % macro]
+    for arg in args:
+        result.append('{%s}' % arg)
+    return ''.join(result)
+
 
 def sqrt(arg1, arg2):
-	result = ['\\sqrt']
-	if arg2 != '2':
-		result.append('[%s]' % arg2)
-	result.append('{%s}' % arg1)
-	return ''.join(result)
+    result = ['\\sqrt']
+    if arg2 != '2':
+        result.append('[%s]' % arg2)
+    result.append('{%s}' % arg1)
+    return ''.join(result)
+
 
 def power(x, y):
-	return '%s^{%s}' % (x, y)
+    return '%s^{%s}' % (x, y)
 
-arith1 = { \
-	'plus' : [binaryOperator, '+', '#1', '#2'], 	\
-	'unary_minus': [unaryOperator, '-', '#1'], 		\
-	'times' : [binaryOperator, '*', '#1', '#2'], 	\
-	'power': [power, '#1', '#2'], 					\
-	'divide': [latexMacro, '\\frac', '#1', '#2'], 	\
-	'root' : [sqrt, '#1', '#2' ], \
-	'minus' : [binaryOperator, '-', '#1', '#2']		\
-}\
+arith1 = {
+    'root': [sqrt, '#1', '#2'],
+    'power': [power, '#1', '#2'],
+    'plus': [binaryOperator, '+', '#1', '#2'],
+    'unary_minus': [unaryOperator, '-', '#1'],
+    'minus': [binaryOperator, '-', '#1', '#2'],
+    'times': [binaryOperator, '*', '#1', '#2'],
+    'divide': [latexMacro, '\\frac', '#1', '#2'],
+}
 
-nums1 = {'pi' : '\\pi'}
+nums1 = {'pi': '\\pi'}
+
 
 class OpenMath2Latex(object):
 
-	contentDicts = {}
+    contentDicts = {}
 
-	def __init__(self):
-		self.contentDicts['arith1'] = arith1
-		self.contentDicts['nums1'] = nums1
+    def __init__(self):
+        self.contentDicts['nums1'] = nums1
+        self.contentDicts['arith1'] = arith1
 
-	def translate(self, openMathXML):
-		dom = parseString(openMathXML)
+    def translate(self, openMathXML):
+        dom = parseString(openMathXML)
 
-		omobj = dom.firstChild
+        omobj = dom.firstChild
 
-		if omobj.localName != OMOBJ:
-			logger.warn('Expected %s but found %s.  Are you sure this is open math',
-				 		 OMOBJ, omobj.localName)
-			return '\\Unknown{%s}' % (omobj)
+        if omobj.localName != OMOBJ:
+            logger.warn('Expected %s but found %s.  Are you sure this is open math',
+                        OMOBJ, omobj.localName)
+            return '\\Unknown{%s}' % (omobj)
 
-		oma = self.getChild(omobj, OMA)
-		handler = self.handleOMA
-		if oma == None or oma.localName != OMA:
-			if hasattr(self, 'handle' + omobj.childNodes[0].localName):
-				handler = getattr(self, 'handle' + omobj.childNodes[0].localName)
-				oma = omobj.childNodes[0]
-			else:
-				logger.warn('Expected %s but found %s', OMA, getattr(oma, 'localName', None))
-				return '\\Unknown{%s}' % (oma)
+        oma = self.getChild(omobj, OMA)
+        handler = self.handleOMA
+        if oma is None or oma.localName != OMA:
+            handler = 'handle' + omobj.childNodes[0].localName
+            if hasattr(self, handler):
+                handler = getattr(self, handler)
+                oma = omobj.childNodes[0]
+            else:
+                logger.warn('Expected %s but found %s',
+                            OMA,
+                            getattr(oma, 'localName',None))
+                return '\\Unknown{%s}' % (oma)
 
-		return '$%s$' % handler(oma)
+        return '$%s$' % handler(oma)
 
-	def getChild(self, node, childName):
-		for child in node.childNodes:
-			if child.localName == childName:
-				return child
-		return None
+    def getChild(self, node, childName):
+        for child in node.childNodes or ():
+            if child.localName == childName:
+                return child
+        return None
 
-	def handleOMS(self, node):
-		"""
-		Returns a tuple of the contentdictionary
-		add operator name.
+    def handleOMS(self, node):
+        """
+        Returns a tuple of the contentdictionary
+        add operator name.
 
-		<OMS cd="arith1" name="unary_minus"/>
-		returns
-		(arith1, unary_minus)
+        <OMS cd="arith1" name="unary_minus"/>
+        returns
+        (arith1, unary_minus)
 
-		e.g.
-		"""
+        e.g.
+        """
 
-		cd = node.getAttribute('cd')
-		name = node.getAttribute('name')
-		return (cd, name)
+        cd = node.getAttribute('cd')
+        name = node.getAttribute('name')
+        return (cd, name)
 
-	def handleOMA(self, node):
-		# First child should be the operator
-		children = node.childNodes
+    def handleOMA(self, node):
+        # First child should be the operator
+        children = node.childNodes
 
-		translator = None
-		args = None
-		possibleArgs = []
+        args = None
+        translator = None
+        possibleArgs = []
 
-		for child in children:
+        for child in children or ():
 
-			if child.localName == OMS:
+            if child.localName == OMS:
 
-				cdname, opname = self.handleOMS(child)
-				cd = self.contentDicts[cdname]
+                cdname, opname = self.handleOMS(child)
+                cd = self.contentDicts[cdname]
 
-				content = None
-				if cd:
-					content = cd[opname]
+                content = None
+                if cd:
+                    content = cd[opname]
 
-				if content == None:
-					logger.warn('Unknown content for %s:%s', cdname, opname)
-					return '\\Unknowncontent{%s}{%s}' % (cdname, opname)
+                if content is None:
+                    logger.warn('Unknown content for %s:%s', cdname, opname)
+                    return '\\Unknowncontent{%s}{%s}' % (cdname, opname)
 
-				if isinstance(content, basestring):
-					possibleArgs.append(content)
-					continue
+                if isinstance(content, basestring):
+                    possibleArgs.append(content)
+                    continue
 
-				translator = content[0]
-				args = content[1:]
+                translator = content[0]
+                args = content[1:]
 
-			elif child.localName == OMI:
-				possibleArgs.append(self.handleOMI(child))
+            elif child.localName == OMI:
+                possibleArgs.append(self.handleOMI(child))
 
-			elif child.localName == OMV:
-				possibleArgs.append(self.handleOMV(child))
+            elif child.localName == OMV:
+                possibleArgs.append(self.handleOMV(child))
 
-			elif child.localName == OMA:
-				possibleArgs.append(self.handleOMA(child))
+            elif child.localName == OMA:
+                possibleArgs.append(self.handleOMA(child))
 
-			elif child.localName == OMF:
-				possibleArgs.append(self.handleOMF(child))
-			else:
-				continue
+            elif child.localName == OMF:
+                possibleArgs.append(self.handleOMF(child))
+            else:
+                continue
 
-		if not translator:
-			logger.warn('No operator found')
-			return '\\NoOperatorFound{%s}' % (node)
+        if not translator:
+            logger.warn('No operator found')
+            return '\\NoOperatorFound{%s}' % (node)
 
-		translatorArgs = []
+        translatorArgs = []
 
-		for arg in args or ():
-			if arg[0] == '#':
-				translatorArgs.append(possibleArgs.pop(0))
-			else:
-				translatorArgs.append(arg)
+        for arg in args or ():
+            if arg[0] == '#':
+                translatorArgs.append(possibleArgs.pop(0))
+            else:
+                translatorArgs.append(arg)
 
-		return self.executeTranslator(translator, translatorArgs)
+        return self.executeTranslator(translator, translatorArgs)
 
-	def executeTranslator(self, func, args):
-		return func(*args)
+    def executeTranslator(self, func, args):
+        return func(*args)
 
-	def handleOMF(self, node):
-		"""
-		OMF has a dec attribute that contains
-		the value.  e.g. <OMF decimal="40000"/>
-		"""
-		return node.getAttribute('dec')
+    def handleOMF(self, node):
+        """
+        OMF has a dec attribute that contains
+        the value.  e.g. <OMF decimal="40000"/>
+        """
+        return node.getAttribute('dec')
 
-	def handleOMV(self, node):
-		"""
-		OMV has a name attribute that contains
-		the variable name.  e.g. <OMV name="a"/>
-		"""
-		return node.getAttribute('name')
+    def handleOMV(self, node):
+        """
+        OMV has a name attribute that contains
+        the variable name.  e.g. <OMV name="a"/>
+        """
+        return node.getAttribute('name')
 
-	def handleOMI(self, node):
-		"""
-		OMI should have on child that is a text node
-		e.g. <OMI>4</OMI>
-		"""
-		return node.firstChild.data
+    def handleOMI(self, node):
+        """
+        OMI should have on child that is a text node
+        e.g. <OMI>4</OMI>
+        """
+        return node.firstChild.data
--- src/nti/openmath/tests/test_openmath.py
+++ src/nti/openmath/tests/test_openmath.py
@@ -15,68 +15,71 @@ import unittest
 
 from nti.openmath import OpenMath2Latex
 
+
 class TestAssessment(unittest.TestCase):
 
-	inputDir = 'openmath'
+    inputDir = 'openmath'
 
-	def setUp(self):
-		self.translator = OpenMath2Latex()
+    def setUp(self):
+        self.translator = OpenMath2Latex()
 
-	def test_simplemath(self):
-		result = self.translateOpenMath('simplemath.xml')
-		assert_that(result, is_('$31 + 3$'))
+    def test_simplemath(self):
+        result = self.translateOpenMath('simplemath.xml')
+        assert_that(result, is_('$31 + 3$'))
 
-		result = self.translateOpenMath('simplemath2.xml')
-		assert_that(result, is_('$31 - 3$'))
+        result = self.translateOpenMath('simplemath2.xml')
+        assert_that(result, is_('$31 - 3$'))
 
-		result = self.translateOpenMath('simplemath3.xml')
-		assert_that(result, is_('$31 * 3$'))
+        result = self.translateOpenMath('simplemath3.xml')
+        assert_that(result, is_('$31 * 3$'))
 
-		result = self.translateOpenMath('simplemath4.xml')
-		assert_that(result, is_('$-31$'))
+        result = self.translateOpenMath('simplemath4.xml')
+        assert_that(result, is_('$-31$'))
 
-	def test_mathwithvars(self):
-		result = self.translateOpenMath('varmath.xml')
-		assert_that(result, is_('$x + 3$'))
+    def test_mathwithvars(self):
+        result = self.translateOpenMath('varmath.xml')
+        assert_that(result, is_('$x + 3$'))
 
-		result = self.translateOpenMath('varmath2.xml')
-		assert_that(result, is_('$x - 3$'))
+        result = self.translateOpenMath('varmath2.xml')
+        assert_that(result, is_('$x - 3$'))
 
-		result = self.translateOpenMath('varmath3.xml')
-		assert_that(result, is_('$x * 3$'))
+        result = self.translateOpenMath('varmath3.xml')
+        assert_that(result, is_('$x * 3$'))
 
-		result = self.translateOpenMath('varmath4.xml')
-		assert_that(result, is_('$-x$'))
+        result = self.translateOpenMath('varmath4.xml')
+        assert_that(result, is_('$-x$'))
 
-	def test_basicmacros(self):
-		result = self.translateOpenMath('macromath.xml')
-		assert_that(result, is_('$\\frac{1}{2}$'))
+    def test_basicmacros(self):
+        result = self.translateOpenMath('macromath.xml')
+        assert_that(result, is_('$\\frac{1}{2}$'))
 
-		result = self.translateOpenMath('macromath2.xml')
-		assert_that(result, is_('$\\frac{1}{x}$'))
+        result = self.translateOpenMath('macromath2.xml')
+        assert_that(result, is_('$\\frac{1}{x}$'))
 
-		result = self.translateOpenMath('macromath3.xml')
-		assert_that(result, is_('$\\sqrt{x}$'))
+        result = self.translateOpenMath('macromath3.xml')
+        assert_that(result, is_('$\\sqrt{x}$'))
 
-		result = self.translateOpenMath('macromath4.xml')
-		assert_that(result, is_('$\\sqrt[3]{64}$'))
+        result = self.translateOpenMath('macromath4.xml')
+        assert_that(result, is_('$\\sqrt[3]{64}$'))
 
-	def test_symbol(self):
-		result = self.translateOpenMath('symbol.xml')
-		assert_that(result, is_('$\\frac{25 * \\pi}{2}$'))
+    def test_symbol(self):
+        result = self.translateOpenMath('symbol.xml')
+        assert_that(result, is_('$\\frac{25 * \\pi}{2}$'))
 
-	def test_complex(self):
-		result = self.translateOpenMath('complexmath.xml')
-		assert_that(result, is_('$\\sqrt[3]{31} + \\frac{q^{2} - 1}{q - 1}$'))
+    def test_complex(self):
+        result = self.translateOpenMath('complexmath.xml')
+        assert_that(result, is_('$\\sqrt[3]{31} + \\frac{q^{2} - 1}{q - 1}$'))
 
-	def test_float(self):
-		result = self.translateOpenMath('floatmath.xml')
-		assert_that(result, is_('$40000.00 + 3$'))
+    def test_float(self):
+        result = self.translateOpenMath('floatmath.xml')
+        assert_that(result, is_('$40000.00 + 3$'))
 
-	def translateOpenMath(self, file_name):
-		xml = open(os.path.join( os.path.dirname(__file__), 'openmath', file_name))
-		try:
-			result = self.translator.translate(xml.read())
-		finally:
-			xml.close()
-		return result
+    def translateOpenMath(self, file_name):
+        xml = open(os.path.join(os.path.dirname(__file__),
+                                'openmath',
+                                file_name))
+        try:
+            result = self.translator.translate(xml.read())
+        finally:
+            xml.close()
+        return result
